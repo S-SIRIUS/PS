2928bdc704d7efcc2893b88bfef7164d6fb79ac8
S-SIRIUS <sirius5b8b@gmail.com>
2024-02-27 20:03:04 +0900

    [feat]0227

--- test.py
+++ test.py
@@ -1,9 +1,3 @@
-n, m = map(int, input().split())
-fields=[]
-fields.append(list(map(int, input().split())))
-for i in range(0, n):
-    if(2 in fields[i]):
-        target_x = i
-        target_y = fields[i].index(2)
-        break
-print(target_x, target_y)
+a=[1,2,3,4,5]
+a.remove(1)
+print(a)
