5f7b2da60e7898d1c4dfdabb4f931d55a04276b3
S-SIRIUS <sirius5b8b@gmail.com>
2024-09-16 21:29:12 +0900

    [feat]0916

--- 문자열/BOJ_5525.py
+++ 문자열/BOJ_5525.py
@@ -1,30 +1,24 @@
 #IOIOI(실버1)
-def check(target, s, t_len, m):
-    count=0
+def check(n, m, s):
+
     i=0
-    while i < m-t_len+1:
-        if s[i] == target[0]:
-            for j in range(1, t_len):
-                if s[i+j] == target[j]:
-                    if j == t_len-1:
-                        count+=1
-                        break
-                    continue
-                else:
-                    i+=(j+1)
-                    break
-        i+=1                
-    return count
+    count=0
+    answer=0
+    while i < m:
+        if s[i:i+3] == "IOI":
+            count+=1
+            if count == n:
+                answer+=1
+                count-=1
+            i+=2
+        else:
+            count=0
+            i+=1
+    return answer
 
 n = int(input())
 m = int(input())
 s = input()
-t_len = n*2+1
-target=""
-for i in range(t_len):
-    if(i%2==1):
-        target+="O"
-    else:
-        target+="I"
-count = check(target, s, t_len, m)
-print(count)
+print(check(n,m,s))
+
+
