550bfc4dd0dfa8f08d15b2016ef0fdc57ffdc60b
S-SIRIUS <sirius5b8b@gmail.com>
2024-10-09 21:12:22 +0900

    [feat]10089

--- test.py
+++ test.py
@@ -1,4 +1,22 @@
-a=[1,2,3]
-b=[4,5,6]
-print(set(a))
-print(set(a)-set(b))
+import sys
+sys.setrecursionlimit(10**7)
+
+answer=0
+cases=[1, 2]
+def dfs(total, n):
+    global answer
+    global cases
+    
+    if total==n:
+        answer+=1
+        return
+    elif total>n:
+        return
+    else:
+        for i in cases:
+            dfs(total + i, n)
+
+n = int(input())
+for case in cases:
+    dfs(case, n)  
+print(answer % 1234567)
