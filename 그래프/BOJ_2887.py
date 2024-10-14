1b178deb226158213ae3f37ac6a9e09b5a426a1b
S-SIRIUS <sirius5b8b@gmail.com>
2024-10-14 21:37:11 +0900

    [feat]1014

--- 그래프/BOJ_2887.py
+++ 그래프/BOJ_2887.py
@@ -1,6 +1,5 @@
 # 행성터널(플래5)
 import sys
-from itertools import combinations
 input = sys.stdin.readline
 
 def find(x):
@@ -19,26 +18,39 @@ def union(a, b):
 def cruscal():
     planet_way.sort()
     answer=0
+    edge_count=0
     for pw in planet_way:
         if find(pw[1]) != find(pw[2]):
             union(pw[1], pw[2])
-            answer+=1
+            answer+=pw[0]
+            edge_count+=1
+            if edge_count == n - 1:  # 조기 종료
+                break
     return answer
 
 
 n = int(input())
-candidates = list(combinations([i for i in range(1, n+1)], 2))
-planets=[()]
-parent = [i for i in range(0, n+1)]
+planets=[]
+parent = [i for i in range(0, n)]
 
+x_list=[]
+y_list=[]
+z_list=[]
 for i in range(n):
     x, y, z = map(int, input().split())
-    planets.append((x,y,z))
+    x_list.append((x,i))
+    y_list.append((y,i))
+    z_list.append((z,i))
+
+x_list.sort()
+y_list.sort()
+z_list.sort()
 
 planet_way=[]
-for cd in candidates:
-    distance = min(abs(planets[cd[0]][0]-planets[cd[1]][0]), abs(planets[cd[0]][1]-planets[cd[1]][1]),
-                       abs(planets[cd[0]][2]-planets[cd[1]][2])) 
-    planet_way.append((distance, cd[0], cd[1]))
-    
+for cur_list in x_list, y_list, z_list:
+    for i in range(1, n):
+        w1, a = cur_list[i-1]
+        w2, b = cur_list[i]
+        planet_way.append((abs(w1-w2), a, b))
+
 print(cruscal())
